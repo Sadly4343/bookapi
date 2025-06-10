c9226d2ebcfbcc7d02ace4d0f71274342d7352e3
Sadly4343 <loganhartshorn@gmail.com>
2025-06-09 20:33:23 -0600

    updates

--- routes/authors.js
+++ routes/authors.js
@@ -1,15 +1,22 @@
 const express = require('express');
+
+
+const { authorValidationRules, validate } = require('../validation/validation')
+
 const router = express.Router();
 
+
 const authorsController = require('../controllers/authors');
+
+const { isAuthenticated } = require('../validation/authenticate');
 
 router.get('/', authorsController.getAllAuthors);
 
 router.get('/:id', authorsController.getSingleAuthor);
 
-router.post('/', authorsController.createAuthor);
+router.post('/', isAuthenticated, authorValidationRules(), validate, authorsController.createAuthor);
 
-router.put('/:id', authorsController.updateAuthor);
+router.put('/:id', isAuthenticated, authorValidationRules(), validate, authorsController.updateAuthor);
 
 router.delete('/:id', authorsController.deleteAuthor);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 
 //User Validation Steps
-//const passport = require('passport');
+const passport = require('passport');
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
@@ -11,6 +11,15 @@ router.get('/', (req, res) => {
 
 router.use('/books', require('./books'));
 router.use('/authors', require('./authors'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err);}
+        res.redirect('/')
+    })
+})
 
 
 module.exports = router;
